008662c625a4b786701797d622d2b56e222f9183
BaoMyTH <baoomy2701@gmail.com>
2025-08-23 17:54:59 +0700

    my2

---
+++ src/label_Baomy.py
@@ -0,0 +1,13 @@
+import pandas as pd
+
+df = pd.read_csv(r"C:\Users\admin\Desktop\Git_p\customer-support-classification\data_raw\Baomy_tiki_4.csv")
+
+df["label"] = None   # tạo cột label rỗng
+
+for i, row in df.iterrows():
+    print(f"\nFeedback {i}: {row['content']}")
+    label = input("Nhập nhãn cho feedback này: ")
+    df.at[i, "label"] = label
+
+# Sau khi gắn nhãn xong, lưu lại file
+df.to_csv("data_raw/labeled_Baomy_tiki4.csv", index=False)
